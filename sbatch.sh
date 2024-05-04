6e846e5996f8d8cdbe545873e650ecad8618bc56
PraxisUnterwegs <zhuofan.jiang2233@gmail.com>
2024-05-04 16:44:12 +0200

    HPC-Prep-train

---
+++ sbatch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/zsh
+ 
+#SBATCH --job-name=3DCDC-NAS
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=64G
+#SBATCH --output=output.%J.txt
+#SBATCH --time=08:00:00
+ 
+export CONDA_ROOT=$HOME/anaconda3 
+export PATH="$CONDA_ROOT/bin:$PATH"
+ 
+source activate pytorch222
+ 
+module load CUDA
+echo; export; echo; nvidia-smi; echo
+ 
+./AutoGesture/Multi_modality/run_hpc.sh
