476f3af8a529bf464b4bf357900980b44384d26e
PraxisUnterwegs <zhuofan.jiang2233@gmail.com>
2024-05-06 10:57:15 +0200

    gpu8

--- AutoGesture/Multi_modality/run_hpc.sh
+++ AutoGesture/Multi_modality/run_hpc.sh
@@ -2,6 +2,6 @@
 MODALITY=MK
 LAYER=AutoGesture_RGBD_Con_shared_DiffChannels
 #LAYER=18
-GPU_IDS=0,1
+GPU_IDS=0,1,2,3,4,5,6,7
 FRAME=32
 python -u train_AutoGesture_CDC_RGBD_sgd_12layers.py -m train --config config_hpc.yml -l $LAYER -t $MODALITY -g $GPU_IDS  | tee ./log/$LAYER-$MODALITY-$FRAME.log
